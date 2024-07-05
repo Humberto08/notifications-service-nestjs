4cd3c84e3134106892d415a69bc37ebc9552cbb6
Humberto Luciano <humberto_luciano@hotmail.com>
2024-07-05 12:25:53 -0300

    feat: test: notification

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,17 +1,19 @@
 import { Content } from "./content";
 
-test('it should be able to create a notification content', () => {
-    const content = new Content('Você recebeu uma solicitação de amizade');
+describe('Notification content', () => {
+    it('should be able to create a notification content', () => {
+        const content = new Content('Você recebeu uma solicitação de amizade');
+        
+        expect(content).toBeTruthy();
+    });
     
-    expect(content).toBeTruthy();
-});
-
-test('it should not be able to create a notification content with less than 5 characters', () => {
-    expect(() => new Content('1234')).toThrow();
-
-});
-
-test('it should not be able to create a notification content with more than 240 characters', () => {
-    expect(() => new Content('a'.repeat(241))).toThrow();
-
-});
+    it('should  not be able to create a notification content with less than 5 characters', () => {
+        expect(() => new Content('1234')).toThrow();
+    
+    });
+    
+    it('should not be able to create a notification content with more than 240 characters', () => {
+        expect(() => new Content('a'.repeat(241))).toThrow();
+    
+    });
+})
